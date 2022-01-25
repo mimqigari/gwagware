edea954270db3dcdf5dba26b323a122e7f9ed755
Mubarak Maigari <tha_rugged@yahoo.com>
2022-01-25 01:25:05 +0100

    Minimun Viable Product

--- db/schema.rb
+++ db/schema.rb
@@ -67,7 +67,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_213324) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "phone"
-    t.boolean "admin"
+    t.boolean "admin", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
