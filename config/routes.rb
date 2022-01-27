e87aa4e6d9b5bc6024805610f18597d7212efb2f
Mubarak Maigari <tha_rugged@yahoo.com>
2022-01-27 20:40:18 +0100

    Test Floi

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :blogs
   devise_for :users
   resources :posts
-  devise_for :members
+
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
